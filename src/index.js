2e02322c0ede1cb435acfb35b819fc310b11c5bf
zuoxianjin <zuoxianjin@achexian.cn>
2016-11-08 22:55:33 +0800

    增加线上线下隔离

--- src/index.js
+++ src/index.js
@@ -51,38 +51,44 @@
       /*=require runtime.js */
 
       win.BDWMMonitor={
-          /**
-           * 初始化
-           * @param use              {Array} 使用的功能模块，net:网络监测；perf:性能监测；runtime:运行时监控
-           * @param reportGlobalData {Object}
-           * @param reportWhiteList  {Array} 数据上报白名单，根据url匹配的正则，不在白名单的页面将不会上报数据（目前仅作用于性能数据上报）
-           */
-          init:function(use,reportGlobalData,reportWhiteList){
-              if(!Utils.isArr(use) || !use.length){
-                  return;
-              }
+        /**
+         * 初始化
+         * @param use              {Array} 使用的功能模块，net:网络监测；perf:性能监测；runtime:运行时监控
+         * @param reportGlobalData {Object}
+         * @param reportWhiteList  {Array} 数据上报白名单，根据url匹配的正则，不在白名单的页面将不会上报数据（目前仅作用于性能数据上报）
+         */
+        init:function(use,reportGlobalData,reportWhiteList){
+            if(!Utils.isArr(use) || !use.length){
+                return
+            }
 
-              reportor=new modules.Reportor(defaultReportUrl,{
-                  platform:reportGlobalData.platform,
-                  app:reportGlobalData.app,
-                  channel:reportGlobalData.channel,
-                  wid:reportGlobalData.wid,
-                  city_id:reportGlobalData.city_id,
-                  protocol:location.protocol.substr(0,location.protocol.length-1)
-              });
+            // 只统计线上环境
+            // 用这种策略是否合适或具备通用性有待商榷
+            if(["443","80",""].indexOf(location.port)==-1){
+                return
+            }
 
-              if(use.indexOf("net")>-1){
-                  modules.Net.init();
-                  resourceLoadAdapter(true);
-                  resourceLoadAdapter(false);
-              }
-              if(use.indexOf("perf")>-1){
-                  modules.Perf.init(reportWhiteList);
-              }
-              if(use.indexOf("runtime")>-1){
-                  modules.Runtime.init();
-              }
-          }
-      }
+            reportor=new modules.Reportor(defaultReportUrl,{
+                platform:reportGlobalData.platform,
+                app:reportGlobalData.app,
+                channel:reportGlobalData.channel,
+                wid:reportGlobalData.wid,
+                city_id:reportGlobalData.city_id,
+                protocol:location.protocol.substr(0,location.protocol.length-1)
+            });
+
+            if(use.indexOf("net")>-1){
+                modules.Net.init();
+                resourceLoadAdapter(true);
+                resourceLoadAdapter(false);
+            }
+            if(use.indexOf("perf")>-1){
+                modules.Perf.init(reportWhiteList);
+            }
+            if(use.indexOf("runtime")>-1){
+                modules.Runtime.init();
+            }
+        }
+    }
 
   })(window);
